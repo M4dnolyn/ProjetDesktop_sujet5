0871fb89da296c620a3c0486d101a013683a0c7c
M4dnolyn <M4dnolyn@gmail.com>
2026-07-20 17:12:58 +0000

    J2-Icons: icône fenêtre + logo login/home + --icon jpackage

--- src/main/java/tg/univlome/epl/pharmarciemanagement/PharmarcieManagement.java
+++ src/main/java/tg/univlome/epl/pharmarciemanagement/PharmarcieManagement.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import tg.univlome.epl.pharmarciemanagement.dao.DatabaseConnection;
 
@@ -16,6 +17,7 @@ public class PharmarcieManagement extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/css/application.css").toExternalForm());
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/logo.png")));
         primaryStage.setTitle("Gestion de Pharmacie");
         primaryStage.setScene(scene);
         primaryStage.show();
