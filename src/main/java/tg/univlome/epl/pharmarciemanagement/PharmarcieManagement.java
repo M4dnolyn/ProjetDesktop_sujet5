dfacab101ba1b160f99cdb1da021319af5ee7a7e
M4dnolyn <M4dnolyn@gmail.com>
2026-07-22 22:39:00 +0000

    Fix jpackage split-package crash : exclure JavaFX de l'uber-JAR + icônes PNG uniquement

--- src/main/java/tg/univlome/epl/pharmarciemanagement/PharmarcieManagement.java
+++ src/main/java/tg/univlome/epl/pharmarciemanagement/PharmarcieManagement.java
@@ -17,26 +17,15 @@ public class PharmarcieManagement extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/css/application.css").toExternalForm());
-        // Load platform-appropriate icon: prefer .ico on Windows, PNG multires on Linux/others
-        String os = System.getProperty("os.name").toLowerCase();
         try {
-            if (os.contains("win")) {
-                java.io.InputStream icoStream = getClass().getResourceAsStream("/images/icon.ico");
-                if (icoStream != null) primaryStage.getIcons().add(new Image(icoStream));
-                java.io.InputStream logoStream = getClass().getResourceAsStream("/images/logo.png");
-                if (logoStream != null) primaryStage.getIcons().add(new Image(logoStream));
-            } else {
-                // Prefer higher-resolution PNGs on Linux/macOS
-                java.io.InputStream png256 = getClass().getResourceAsStream("/images/icon-256.png");
-                java.io.InputStream png128 = getClass().getResourceAsStream("/images/icon-128.png");
-                java.io.InputStream png64 = getClass().getResourceAsStream("/images/icon-64.png");
-                if (png256 != null) primaryStage.getIcons().add(new Image(png256));
-                if (png128 != null) primaryStage.getIcons().add(new Image(png128));
-                if (png64 != null) primaryStage.getIcons().add(new Image(png64));
-                // fallback to logo.png
-                java.io.InputStream logoStream = getClass().getResourceAsStream("/images/logo.png");
-                if (logoStream != null) primaryStage.getIcons().add(new Image(logoStream));
-            }
+            java.io.InputStream png256 = getClass().getResourceAsStream("/images/icon-256.png");
+            java.io.InputStream png128 = getClass().getResourceAsStream("/images/icon-128.png");
+            java.io.InputStream png64 = getClass().getResourceAsStream("/images/icon-64.png");
+            if (png256 != null) primaryStage.getIcons().add(new Image(png256));
+            if (png128 != null) primaryStage.getIcons().add(new Image(png128));
+            if (png64 != null) primaryStage.getIcons().add(new Image(png64));
+            java.io.InputStream logoStream = getClass().getResourceAsStream("/images/logo.png");
+            if (logoStream != null) primaryStage.getIcons().add(new Image(logoStream));
         } catch (Exception e) {
             // silent fallback
         }
